d9d1edfd48bacb5034db973b8504dae83415014c
Cristhiam Poma <yoshicr7.cpm@gmail.com>
2022-01-19 18:47:30 -0400

    create project

---
+++ src/main/java/bo/vulcan/yoshi/Operations.java
@@ -0,0 +1,21 @@
+package bo.vulcan.yoshi;
+
+public class Operations {
+
+    public static Integer add(Integer a, Integer b){
+        return a+b;
+    }
+
+    public static Integer subtract(Integer a, Integer b){
+        return a-b;
+    }
+
+    public static Integer multiply(Integer a, Integer b){
+        return a*b;
+    }
+
+    public static Integer divide(Integer a, Integer b){
+        return a/b;
+    }
+
+}
